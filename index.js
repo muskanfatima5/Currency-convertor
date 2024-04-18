1451945ac158473ab71f2cd6357e271794a1c7de
muskanfatima5 <muskanf217@gmail.com>
2024-04-19 02:00:42 +0500

    Push my currency convertor to github

---
+++ index.js
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let exchange_rate = {
+    "USD": 1,
+    "EURO": 0.9,
+    "JYP": 113,
+    "CAD": 1.3,
+    "AUD": 1.65,
+    "PKR": 277.7, //Pakistani Rupees
+};
+let user_answer = await inquirer.prompt([
+    {
+        name: "from_currency",
+        message: "Select the currency to convert from",
+        type: "list",
+        choices: ["USD", "EURO", "JYP", "CAD", "AUD", "PKR"]
+    },
+    {
+        name: "to_currency",
+        message: "Select the currency to convert into",
+        type: "list",
+        choices: ["USD", "EURO", "JYP", "CAD", "AUD", "PKR"]
+    },
+    {
+        name: "amount",
+        message: "Enter the amount to convert",
+        type: "input"
+    }
+]);
+let from_amount = exchange_rate[user_answer.from_currency];
+let to_amount = exchange_rate[user_answer.to_currency];
+let amount = user_answer.amount;
+//Formula of conversion
+let base_amount = amount / from_amount;
+let converted_amount = base_amount * to_amount;
+console.log(converted_amount.toFixed(2));
